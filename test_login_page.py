d4158473cbe6bb7cf0c2a218c63c2c7edc638572
Jimmy McGill <f2mn1cj42@mozmail.com>
2024-04-08 16:52:18 +0300

    Create new object LoginPage and add tests for it. Create new locators for LoginPage.

---
+++ test_login_page.py
@@ -0,0 +1,19 @@
+from .Pages.login_page import LoginPage
+
+link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+
+def test_should_be_login_url(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+def test_guest_should_see_login_form(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_guest_should_see_register_form(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
